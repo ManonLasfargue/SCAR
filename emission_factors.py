4a0618785aa47dc32967d14202b1e4adea98b206
Manon Lasfargue <manon1.lasfargue@gmail.com>
2026-01-14 15:01:00 +0100

    Add files via upload

---
+++ emission_factors.py
@@ -0,0 +1,42 @@
+FACTORS = {
+    # --- Mobilité ---
+    "flight_short": 0.255,
+    "flight_medium": 0.145,
+    "flight_long": 0.133,
+    "train_tgv": 0.003,
+    "train_ter": 0.007,
+    "car_diesel": 0.25,
+    "car_essence": 0.23,
+    "carpool": 0.083,
+    "bus": 0.10,
+    "metro": 0.005,
+
+    # --- Énergie ---
+    "electricity": 0.079,
+    "gas": 0.227,
+    "fioul": 0.300,
+    "heating_network": 0.180,
+
+    # --- Numérique ---
+    "laptops": 300,
+    "desktop": 450,
+    "screen": 100,
+    "server": 1600,
+    "cloud": 120,
+    "video_conf": 0.055,
+    "email": 0.035,
+    "llm_usage": 0.001,
+    "ai_training": 3.0,
+
+    # --- Alimentation ---
+    "meat_meals": 5.0,
+    "white_meat_meals": 2.0,
+    "veg_meals": 1.0,
+    "coffee": 0.07,
+
+    # --- Hébergement / Événements ---
+    "hotel_night": 6.5,
+    "airbnb_night": 3.5,
+    "conference_day": 15.0,
+    "conference_meal": 4.0
+}
